094567efc558748bcbfd722397b95ccda00123d1
Hao Bui <haobp.dev@gmail.com>
2023-11-29 02:04:44 -0600

    apply redux-persist to keep the cart from removed when refresh the page

--- src/store/store.js
+++ src/store/store.js
@@ -3,26 +3,30 @@ import {
   legacy_createStore as createStore,
   applyMiddleware,
 } from 'redux';
-//import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('current State: ', store.getState());
-
-  next(action);
-
-  console.log('next State: ', store.getState());
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
 };
 
-const middleWares = [loggerMiddleware];
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
+  Boolean
+);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
