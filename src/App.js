18ac19b67749ce4092169f803336e8a98c5259d3
Hao Bui <haobp.dev@gmail.com>
2023-12-05 01:18:37 -0600

    switch userReducer to Redux Toolkit

--- src/App.js
+++ src/App.js
@@ -22,8 +22,10 @@ const App = () => {
       if (user) {
         createUserDocumentFromAuth(user);
       }
-      console.log(setCurrentUser(user));
-      dispatch(setCurrentUser(user));
+      const pickedUser =
+        user && (({ accessToken, email }) => ({ accessToken, email }))(user);
+      console.log(setCurrentUser(pickedUser));
+      dispatch(setCurrentUser(pickedUser));
     });
 
     return unsubscribe;
